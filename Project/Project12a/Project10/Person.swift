d7c178fc01b693868d814e4cc606ac56f31f59ff
Ramanan R Rengaraj <10895144+rrramanan@users.noreply.github.com>
2021-04-29 12:54:39 +0200

    Project 12a - UserDefaults

---
+++ Project/Project12a/Project10/Person.swift
@@ -0,0 +1,31 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by R R on 02/01/21.
+//
+
+import UIKit
+
+class Person: NSObject,NSCoding {
+  
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name,forKey: "name")
+        coder.encode(image,forKey: "image")
+    }
+    
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+   
+    
+}
